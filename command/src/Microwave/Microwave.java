5ad8fb10f3ee4713b0dce2708281b6f56e4134a7
litemars <44295342+litemars@users.noreply.github.com>
2019-11-14 22:01:56 +0100

    Microwave

---
+++ command/src/Microwave/Microwave.java
@@ -0,0 +1,15 @@
+public class Microwave extends Device{
+
+
+    private float temperature;
+
+    public Microwave(){
+        super();
+        this.Temperature=0;
+    }
+
+    public setTemperature(flaot temperature){
+        this.temperature=temperature;
+    }
+
+}
