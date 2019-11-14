bcfff2625d35cf4f35236e3af427cd11cd791fc3
Lite_Mars <maxmassi12@gmail.com>
2019-11-14 22:04:14 +0100

    delete

--- command/src/Microwave.java
+++
@@ -1,9 +0,0 @@
-public class Microwave implements OnOffSwitchable {
-
-    public void switchOn() {
-        return;
-    }
-    public void switchOff() {
-        return;
-    }
-}
--- command/src/Oven.java
+++
@@ -1,9 +0,0 @@
-public class Oven implements OnOffSwitchable {
-
-    public void switchOn() {
-        return;
-    }
-    public void switchOff() {
-        return;
-    }
-}
